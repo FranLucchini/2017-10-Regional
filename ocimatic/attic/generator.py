3d8843de34c925ec0b99126ac504c89924b25cec
Rodrigo Alonso <rodrigo.alonso.o@gmail.com>
2017-10-01 14:18:53 -0700

    Testplan fixes

--- ocimatic/attic/generator.py
+++ ocimatic/attic/generator.py
@@ -5,10 +5,10 @@ import random
 random.seed(hash(sys.argv[1]))
 
 exacto = int(sys.argv[2])
-max_espera = long(sys.argv[3])
+max_espera = int(sys.argv[3])
 dias_inicio = int(sys.argv[4])
 
 if exacto == 0:
   print(max_espera, dias_inicio)
-else
+else:
   print((random.getrandbits(128) % max_espera) + 1, random.randint(1, dias_inicio))
