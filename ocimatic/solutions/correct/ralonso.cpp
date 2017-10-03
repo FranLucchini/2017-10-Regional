417d16aa64b8be06dd9c4e4eb9c513426136ff7c
Rodrigo Alonso <rodrigo.alonso.o@gmail.com>
2017-10-03 00:11:04 -0700

    General solution

---
+++ ocimatic/solutions/correct/ralonso.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int w;
+  long long d, r = 0L;
+  cin >> d >> w;
+  if (++w > 4L) {
+    r += 7L - w;
+    w = 0L;
+  }
+  r += d + (w + d) / 5L * 2L;
+  cout << r << endl;
+  return 0;
+}
